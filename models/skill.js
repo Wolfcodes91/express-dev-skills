8ad5549ff0f146987f26639d34c0271b82dad798
Wolfcodes91 <Dave.M.Wolfberg@gmail.com>
2022-01-12 16:23:48 -0800

    it works

--- models/skill.js
+++ models/skill.js
@@ -1,5 +1,5 @@
-const skills = [
-    {id: 125223, skill: 'Get' },
+let skills = [
+    {id: 125223, skill: 'Get'},
     {id: 127904, skill: 'Post'},
     {id: 139608, skill: 'Put'},
     {id: 123456, skill: 'Delete'},
@@ -9,11 +9,30 @@ const skills = [
   module.exports = {
     getAll,
     getOne,
+    create,
+    deleteOne,
+    update,
   };
+
+  function update(id, newSkill) {
+    id = parseInt(id);
+    const skill = skills.find(skill => skill.id === id);
+    Object.assign(skill, newSkill);
+  }
+
+  function deleteOne(id) {
+    id = parseInt(id);
+    skills = skills.filter(skill => skill.id !== id);
+  }
+
+  function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skills.push(skill);
+  }
   
   function getOne(id) {
     id = parseInt(id);
-    return skills.find((skill) => skill.id === id);
+    return skills.find(skill => skill.id === id);
   }
 
   function getAll() {
--- routes/skills.js
+++ routes/skills.js
@@ -4,9 +4,20 @@ var router = express.Router();
 var skillsCtrl = require('../controllers/skills')
 // All routes start with the path of '/todos'
 
-// GET /todos (index functionality - list all todos)
+// GET /skills (index functionality - list all todos)
 router.get('/', skillsCtrl.index);
-// GET /todos/:id (show functionality - show a single todo)
+//GET /skills/new
+router.get('/new', skillsCtrl.new);
+// GET /skills/:id (show functionality - show a single todo)
 router.get('/:id', skillsCtrl.show);
+// POST /skills
+router.post('/', skillsCtrl.create);
+// DELETE /skills/:id
+router.delete('/:id', skillsCtrl.delete);
+// GET /skills/:id/edit (edit functionality)
+router.get('/:id/edit', skillsCtrl.edit)
+// PUT /skills/:id (update functionality)
+router.put('/:id', skillsCtrl.update) 
+
 
 module.exports = router;
